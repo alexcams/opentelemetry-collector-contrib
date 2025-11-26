aeb0550ed95077b564b57085a705498df4ac5288
Álex Cámara <alex.camara@elastic.co>
2025-11-26 09:53:36 +0100

    [pkg/ottl] Add PSliceGetSetter and StandardPSliceGetSetter

--- pkg/ottl/functions.go
+++ pkg/ottl/functions.go
@@ -615,6 +615,19 @@ func (p *Parser[K]) buildArg(argVal value, argType reflect.Type) (any, error) {
 			return nil, err
 		}
 		return newStandardPMapGetter[K](arg)
+	case strings.HasPrefix(name, "PSliceGetSetter"):
+		if argVal.Literal == nil || argVal.Literal.Path == nil {
+			return nil, errors.New("must be a path")
+		}
+		pathGetSetter, err := p.buildGetSetterFromPath(argVal.Literal.Path)
+		if err != nil {
+			return nil, err
+		}
+		stdSliceGetSetter, err := newStandardPSliceGetSetter[K](pathGetSetter)
+		if err != nil {
+			return nil, err
+		}
+		return stdSliceGetSetter, nil
 	case strings.HasPrefix(name, "PSliceGetter"):
 		arg, err := p.newGetter(argVal)
 		if err != nil {
